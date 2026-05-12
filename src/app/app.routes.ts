d6ddc3ded55985f3b0213cabf58d93383d302e40
Gopisainadh-ram <geniusgopisainadh@gmail.com>
2026-05-12 21:15:02 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home')
+        .then(m => m.Home)
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about')
+        .then(m => m.About)
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./pages/products/products')
+        .then(m => m.Products)
+  },
+  {
+    path: 'gallery',
+    loadComponent: () =>
+      import('./pages/gallery/gallery')
+        .then(m => m.Gallery)
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact')
+        .then(m => m.Contact)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ src/app/layouts/website-layout/website-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-website-layout',
+  imports: [],
+  templateUrl: './website-layout.html',
+  styleUrl: './website-layout.scss',
+})
+export class WebsiteLayout {
+
+}
